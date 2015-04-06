d11c67c8fe3fce8a8f6dc4a8786689e9edf6796c
Joachim Baran <joachim.baran@gmail.com>
2015-04-05 20:28:37 -0400

    Basic unit tests.

---
+++ test/libdocument/document.cpp
@@ -0,0 +1,211 @@
+
+#include <gtest/gtest.h>
+
+#include "document.h"
+
+#define LDOC_NULLTYPE long
+
+static const char* ldoc_big_doc_1_1 = "Heading 1";
+// Lorem ipsum, 1500s:
+static const char* ldoc_big_doc_1_2_1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+// Cicero, 45BC:
+static const char* ldoc_big_doc_1_2_2 = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+static const char* ldoc_big_doc_2_1 = "Heading 2";
+static const char* ldoc_big_doc_2_1_1 = "Heading 3";
+static const char* ldoc_big_doc_2_1_2 = "Lorem ipsum... you get the idea!";
+static const char* ldoc_big_doc_2_2_1_1 = "Heading 3, ";
+static const char* ldoc_big_doc_2_2_1_2 = "again";
+static const char* ldoc_big_doc_2_2_1_3 = "!";
+static const char* ldoc_big_doc_2_2_2 = "Introducing some ";
+static const char* ldoc_big_doc_2_2_3 = "emphasized";
+static const char* ldoc_big_doc_2_2_4 = " text!";
+
+static ldoc_doc_t* ldoc_big_doc()
+{
+    ldoc_doc_t* doc = ldoc_doc_new();
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)doc);
+    
+    ldoc_nde_t* nde = ldoc_nde_new(LDOC_NDE_H1);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)nde);
+    
+    doc->rt = nde;
+    
+    ldoc_ent_t* ent = ldoc_ent_new(LDOC_ENT_TXT);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)ent);
+    ent->pld.str = (char*)ldoc_big_doc_1_1;
+    ldoc_nde_ent_push(nde, ent);
+    
+    ldoc_nde_t* nde_ = ldoc_nde_new(LDOC_NDE_PAR);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)nde_);
+    ldoc_nde_dsc_push(nde, nde_);
+    
+    ent = ldoc_ent_new(LDOC_ENT_TXT);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)ent);
+    ent->pld.str = (char*)ldoc_big_doc_1_2_1;
+    ldoc_nde_ent_push(nde_, ent);
+
+    nde_ = ldoc_nde_new(LDOC_NDE_PAR);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)nde_);
+    ldoc_nde_dsc_push(nde, nde_);
+
+    ent = ldoc_ent_new(LDOC_ENT_TXT);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)ent);
+    ent->pld.str = (char*)ldoc_big_doc_1_2_2;
+    ldoc_nde_ent_push(nde_, ent);
+    
+    ldoc_nde_t* nde_h2 = ldoc_nde_new(LDOC_NDE_H2);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)nde_h2);
+    ldoc_nde_dsc_push(nde, nde_h2);
+    nde = nde_h2;
+    
+    ent = ldoc_ent_new(LDOC_ENT_TXT);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)ent);
+    ent->pld.str = (char*)ldoc_big_doc_2_1;
+    ldoc_nde_ent_push(nde, ent);
+
+    nde_ = ldoc_nde_new(LDOC_NDE_H3);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)nde_);
+    ldoc_nde_dsc_push(nde, nde_);
+    nde = nde_;
+    
+    ent = ldoc_ent_new(LDOC_ENT_TXT);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)ent);
+    ent->pld.str = (char*)ldoc_big_doc_2_1_1;
+    ldoc_nde_ent_push(nde, ent);
+    
+    nde_ = ldoc_nde_new(LDOC_NDE_PAR);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)nde_);
+    ldoc_nde_dsc_push(nde, nde_);
+    nde = nde_;
+    
+    ent = ldoc_ent_new(LDOC_ENT_TXT);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)ent);
+    ent->pld.str = (char*)ldoc_big_doc_2_1_2;
+    ldoc_nde_ent_push(nde, ent);
+    
+    nde = ldoc_nde_new(LDOC_NDE_H3);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)nde);
+    ldoc_nde_dsc_push(nde_h2, nde);
+    
+    ent = ldoc_ent_new(LDOC_ENT_TXT);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)ent);
+    ent->pld.str = (char*)ldoc_big_doc_2_2_1_1;
+    ldoc_nde_ent_push(nde, ent);
+
+    ent = ldoc_ent_new(LDOC_ENT_EM1);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)ent);
+    ent->pld.str = (char*)ldoc_big_doc_2_2_1_2;
+    ldoc_nde_ent_push(nde, ent);
+    
+    ent = ldoc_ent_new(LDOC_ENT_TXT);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)ent);
+    ent->pld.str = (char*)ldoc_big_doc_2_2_1_3;
+    ldoc_nde_ent_push(nde, ent);
+    
+    nde_ = ldoc_nde_new(LDOC_NDE_PAR);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)nde_);
+    ldoc_nde_dsc_push(nde, nde_);
+    nde = nde_;
+    
+    ent = ldoc_ent_new(LDOC_ENT_TXT);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)ent);
+    ent->pld.str = (char*)ldoc_big_doc_2_2_2;
+    ldoc_nde_ent_push(nde, ent);
+
+    ent = ldoc_ent_new(LDOC_ENT_EM1);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)ent);
+    ent->pld.str = (char*)ldoc_big_doc_2_2_3;
+    ldoc_nde_ent_push(nde, ent);
+    
+    ent = ldoc_ent_new(LDOC_ENT_TXT);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)ent);
+    ent->pld.str = (char*)ldoc_big_doc_2_2_4;
+    ldoc_nde_ent_push(nde, ent);
+    
+    return doc;
+}
+
+TEST(ldoc_document, empty_document)
+{
+    ldoc_doc_t* doc = ldoc_doc_new();
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)doc);
+    
+    ldoc_doc_free(doc);
+}
+
+TEST(ldoc_document, empty_node)
+{
+    ldoc_nde_t* nde = ldoc_nde_new(LDOC_NDE_PAR);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)nde);
+    // Parent gets assigned when using descend push/shift functions:
+    EXPECT_EQ(LDOC_NDE_NULL, nde->prnt);
+    EXPECT_EQ(LDOC_NDE_PAR, nde->tpe);
+    
+    ldoc_nde_free(nde);
+}
+
+TEST(ldoc_document, empty_entity)
+{
+    ldoc_ent_t* ent = ldoc_ent_new(LDOC_ENT_TXT);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)ent);
+    EXPECT_EQ(LDOC_ENT_TXT, ent->tpe);
+    
+    ldoc_ent_free(ent);
+}
+
+TEST(ldoc_document, small_document)
+{
+    ldoc_doc_t* doc = ldoc_doc_new();
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)doc);
+    
+    ldoc_nde_t* nde = ldoc_nde_new(LDOC_NDE_UA);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)nde);
+    
+    ldoc_ent_t* ent = ldoc_ent_new(LDOC_ENT_TXT);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)ent);
+    
+    EXPECT_EQ(0, nde->dsc_cnt);
+    EXPECT_EQ(0, nde->ent_cnt);
+    ldoc_nde_ent_push(nde, ent);
+    EXPECT_EQ(0, nde->dsc_cnt);
+    EXPECT_EQ(1, nde->ent_cnt);
+    
+    ldoc_doc_free(doc);
+}
+
+TEST(ldoc_document, big_document)
+{
+}
+
+TEST(ldoc_document, format)
+{
+    ldoc_doc_t* doc = ldoc_big_doc();
+    
+    ldoc_vis_nde_ord_t* vis_nde = ldoc_vis_nde_ord_new();
+    vis_nde->vis_setup = ldoc_vis_setup_html;
+    vis_nde->vis_teardown = ldoc_vis_teardown_html;
+    ldoc_vis_nde_uni(&(vis_nde->pre), ldoc_vis_nde_pre_html);
+    ldoc_vis_nde_uni(&(vis_nde->infx), ldoc_vis_nde_infx_html);
+    ldoc_vis_nde_uni(&(vis_nde->post), ldoc_vis_nde_post_html);
+    
+    ldoc_vis_ent_t* vis_ent = ldoc_vis_ent_new();
+    ldoc_vis_ent_uni(vis_ent, ldoc_vis_ent_html);
+    
+    ldoc_ser_t* ser = ldoc_format(doc, vis_nde, vis_ent);
+    
+    printf("%s", ser->sclr.str);
+    
+    ldoc_doc_free(doc);
+}
+
+TEST(ldoc_document, find_by_position)
+{
+
+}
+
+TEST(ldoc_document, find_by_keyword)
+{
+
+}
+
+
---
+++ test/libdocument/trie.cpp
@@ -0,0 +1,121 @@
+
+#include <gtest/gtest.h>
+
+#include "trie.h"
+
+#define LDOC_NULLTYPE long
+
+#define FLORA 1
+#define FAUNA 2
+
+const char* entry_cat = "cat";
+const char* entry_catnip = "catnip";
+const char* entry_rose = "rose";
+
+TEST(ldoc_trie, empty_trie)
+{
+    ldoc_trie_t* trie = ldoc_trie_new();
+    
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)trie);
+    
+    ldoc_trie_free(trie);
+}
+
+TEST(ldoc_trie, small_trie)
+{
+    ldoc_trie_t* trie = ldoc_trie_new();
+    
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)trie);
+    
+    ldoc_trie_anno_t fauna = { FAUNA, NULL };
+    ldoc_trie_anno_t flora = { FLORA, NULL };
+    
+    ldoc_trie_add(trie, entry_cat, EN_ALPH, fauna);
+    ldoc_trie_add(trie, entry_catnip, EN_ALPH, flora);
+    ldoc_trie_add(trie, entry_rose, EN_ALPH, flora);
+    
+    ldoc_trie_free(trie);
+}
+
+TEST(ldoc_trie, lookup)
+{
+    ldoc_trie_nde_t* res;
+    ldoc_trie_t* trie = ldoc_trie_new();
+    
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)trie);
+    
+    ldoc_trie_anno_t fauna = { FAUNA, (void*)123 };
+    ldoc_trie_anno_t flora = { FLORA, LDOC_TRIE_ANNO_NULL.pld };
+    
+    ldoc_trie_add(trie, entry_cat, EN_ALPH, fauna);
+    ldoc_trie_add(trie, entry_catnip, EN_ALPH, flora);
+    ldoc_trie_add(trie, entry_rose, EN_ALPH, flora);
+    
+    // Show the complete trie:
+    // ldoc_trie_dmp(trie);
+
+    res = ldoc_trie_lookup(trie, "ca", false);
+    EXPECT_EQ(NULL, (LDOC_NULLTYPE)res);
+    
+    res = ldoc_trie_lookup(trie, "cat", false);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)res);
+    EXPECT_EQ(FAUNA, res->anno.cat);
+    EXPECT_EQ((void*)123, res->anno.pld);
+    
+    res = ldoc_trie_lookup(trie, "rose", false);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)res);
+    EXPECT_EQ(FLORA, res->anno.cat);
+    EXPECT_EQ(LDOC_TRIE_ANNO_NULL.pld, res->anno.pld);
+    
+    res = ldoc_trie_lookup(trie, "catnip", false);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)res);
+    EXPECT_EQ(FLORA, res->anno.cat);
+    EXPECT_EQ(LDOC_TRIE_ANNO_NULL.pld, res->anno.pld);
+    
+    res = ldoc_trie_lookup(trie, "carrot", false);
+    EXPECT_EQ(NULL, (LDOC_NULLTYPE)res);
+    
+    res = ldoc_trie_lookup(trie, "zebra", false);
+    EXPECT_EQ(NULL, (LDOC_NULLTYPE)res);
+    
+    ldoc_trie_free(trie);
+}
+
+TEST(ldoc_trie, lookup_prefix)
+{
+    ldoc_trie_nde_t* res;
+    ldoc_trie_t* trie = ldoc_trie_new();
+    
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)trie);
+    
+    ldoc_trie_anno_t fauna = { FAUNA, (void*)123 };
+    ldoc_trie_anno_t flora = { FLORA, LDOC_TRIE_ANNO_NULL.pld };
+    
+    ldoc_trie_add(trie, entry_cat, EN_ALPH, fauna);
+    ldoc_trie_add(trie, entry_catnip, EN_ALPH, flora);
+    ldoc_trie_add(trie, entry_rose, EN_ALPH, flora);
+    
+    res = ldoc_trie_lookup(trie, "cat", true);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)res);
+    EXPECT_EQ(FAUNA, res->anno.cat);
+    EXPECT_EQ((void*)123, res->anno.pld);
+    
+    res = ldoc_trie_lookup(trie, "catnip", true);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)res);
+    EXPECT_EQ(FLORA, res->anno.cat);
+    EXPECT_EQ(LDOC_TRIE_ANNO_NULL.pld, res->anno.pld);
+    
+    res = ldoc_trie_lookup(trie, "ca", true);
+    EXPECT_NE(NULL, (LDOC_NULLTYPE)res);
+    EXPECT_EQ(0, res->anno.cat);
+    EXPECT_EQ(NULL, res->anno.pld);
+    
+    res = ldoc_trie_lookup(trie, "carrot", true);
+    EXPECT_EQ(NULL, (LDOC_NULLTYPE)res);
+    
+    res = ldoc_trie_lookup(trie, "zebra", true);
+    EXPECT_EQ(NULL, (LDOC_NULLTYPE)res);
+    
+    ldoc_trie_free(trie);
+}
+
