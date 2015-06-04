e9b4a379926b99e5c453c18b99bace078f5b0ad4
Joachim Baran <joachim.baran@gmail.com>
2015-06-04 08:45:10 -0400

    Updated dependencies in build script; compiling LibDocument example.

--- src/examples/ex1.c
+++ src/examples/ex1.c
@@ -1,7 +1,25 @@
 #include <document.h>
 
-void main()
+int main(int argc, char** argv)
 {
-    ldoc_format(NULL, NULL);
-    printf("main()\n");
+    ldoc_doc_t* doc = ldoc_doc_new();
+    
+    if (!doc)
+        exit(1);
+    
+    ldoc_nde_t* nde = ldoc_nde_new(LDOC_NDE_UA);
+    
+    if (!nde)
+        exit(2);
+        
+    ldoc_nde_dsc_push(doc->rt, nde);
+    
+    ldoc_ent_t* ent = ldoc_ent_new(LDOC_ENT_TXT);
+    
+    if (!ent)
+        exit(3);
+    
+    ldoc_nde_ent_push(nde, ent);
+    
+    ldoc_doc_free(doc);
 }
