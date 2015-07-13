1148bf3169b79ddfcc6dcb9ba8be30bf12972b45
Joachim Baran <joachim.baran@gmail.com>
2015-07-12 23:34:51 -0400

    Emptying node/entity TAILQ lists when freeing document structures.

--- src/lib/document.c
+++ src/lib/document.c
@@ -1687,13 +1687,17 @@ void ldoc_nde_free(ldoc_nde_t* nde)
     TAILQ_FOREACH(ent, &(nde->ents), ldoc_ent_entries)
     {
         ldoc_ent_free(ent);
+        
+        TAILQ_REMOVE(&(nde->ents), ent, ldoc_ent_entries);
     }
     
-    ldoc_nde_t* cld;
+    ldoc_nde_t* dsc;
     
-    TAILQ_FOREACH(cld, &(nde->dscs), ldoc_nde_entries)
+    TAILQ_FOREACH(dsc, &(nde->dscs), ldoc_nde_entries)
     {
-        ldoc_nde_free(cld);
+        ldoc_nde_free(dsc);
+        
+        TAILQ_REMOVE(&(nde->dscs), dsc, ldoc_nde_entries);
     }
     
     free(nde);
